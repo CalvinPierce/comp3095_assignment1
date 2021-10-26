f3ec01317fee506fc87e08d0e7c8a9937492ef0d
CalvinPierce <calvinpierce99@gmail.com>
2021-10-26 14:01:59 -0400

    Added Recipe and User java classes to build database

---
+++ recipe/recipe-data/src/main/java/ca/gbc/comp3095/recipe/model/Recipe.java
@@ -0,0 +1,152 @@
+package ca.gbc.comp3095.recipe.model;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String prepTime;
+    private String cookTime;
+    private String totalTime;
+    @ElementCollection
+    private List<String> ingredients = new ArrayList<String>();
+    private String instructions;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_like",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> likedByUsers = new HashSet<>();
+
+    @OneToMany(mappedBy = "author")
+    private Set<User> author = new HashSet<>();
+
+    public Recipe() {
+    }
+
+    public Recipe(Long id, String name, String prepTime, String cookTime, String totalTime, List<String> ingredients, String instructions) {
+        this.id = id;
+        this.name = name;
+        this.prepTime = prepTime;
+        this.cookTime = cookTime;
+        this.totalTime = totalTime;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    public Recipe(Long id, String name, String prepTime, String cookTime, String totalTime, List<String> ingredients, String instructions, Set<User> likedByUsers, Set<User> author) {
+        this.id = id;
+        this.name = name;
+        this.prepTime = prepTime;
+        this.cookTime = cookTime;
+        this.totalTime = totalTime;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.likedByUsers = likedByUsers;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(String prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public String getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(String cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public String getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(String totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public Set<User> getLikedByUsers() {
+        return likedByUsers;
+    }
+
+    public void setLikedByUsers(Set<User> likedByUsers) {
+        this.likedByUsers = likedByUsers;
+    }
+
+    public Set<User> getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Set<User> author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", prepTime='" + prepTime + '\'' +
+                ", cookTime='" + cookTime + '\'' +
+                ", totalTime='" + totalTime + '\'' +
+                ", ingredients=" + ingredients +
+                ", instructions='" + instructions + '\'' +
+                ", likedByUsers=" + likedByUsers +
+                ", author=" + author +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Recipe recipe = (Recipe) o;
+        return Objects.equals(id, recipe.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ recipe/recipe-data/src/main/java/ca/gbc/comp3095/recipe/model/User.java
@@ -0,0 +1,152 @@
+package ca.gbc.comp3095.recipe.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String postalCode;
+    private String username;
+    private String password;
+
+    @ManyToMany(mappedBy = "likedByUsers")
+    private Set<Recipe> likedRecipes = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private User author;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String address, String postalCode, String username, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(Long id, String firstName, String lastName, String address, String postalCode, String username, String password, Set<Recipe> likedRecipes, User author) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.username = username;
+        this.password = password;
+        this.likedRecipes = likedRecipes;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Recipe> getLikedRecipes() {
+        return likedRecipes;
+    }
+
+    public void setLikedRecipes(Set<Recipe> likedRecipes) {
+        this.likedRecipes = likedRecipes;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", likedRecipes=" + likedRecipes +
+                ", author=" + author +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
