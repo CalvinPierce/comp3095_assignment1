9239bcfdbb56be9344c46d1bd652c25ed514e034
CalvinPierce <calvinpierce99@gmail.com>
2021-11-01 12:13:09 -0400

    Added .java headers

--- recipe/recipe-data/src/main/java/ca/gbc/comp3095/recipe/config/MyUserDetails.java
+++ recipe/recipe-data/src/main/java/ca/gbc/comp3095/recipe/config/MyUserDetails.java
@@ -1,3 +1,13 @@
+/**********************************************************************************
+* Project: < comp3095_assignment1 >
+ * Assignment: < assignment 1 >
+ * Author(s): < Calvin Pierce>
+ * Student Number: < 101253832 >
+ * Date: November 1st 2021
+ * Description: This java file is used to set the user in the security login so,
+ * they can properly use application once logged in.
+ **********************************************************************************/
+
 package ca.gbc.comp3095.recipe.config;
 
 import ca.gbc.comp3095.recipe.model.Role;
--- recipe/recipe-data/src/main/java/ca/gbc/comp3095/recipe/repositories/UserRepository.java
+++ recipe/recipe-data/src/main/java/ca/gbc/comp3095/recipe/repositories/UserRepository.java
@@ -1,3 +1,11 @@
+/**********************************************************************************
+ * Project: < comp3095_assignment1 >
+ * Assignment: < assignment 1 >
+ * Author(s): < Calvin Pierce>
+ * Student Number: < 101253832 >
+ * Date: November 1st 2021
+ * Description: This java file is used to find users in our application.
+ **********************************************************************************/
 package ca.gbc.comp3095.recipe.repositories;
 
 import ca.gbc.comp3095.recipe.model.User;
