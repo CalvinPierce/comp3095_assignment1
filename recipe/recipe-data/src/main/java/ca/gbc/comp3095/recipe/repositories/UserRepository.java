bfc6f6b33b68e43f20f77e6374caa81aae88f4e8
CalvinPierce <calvinpierce99@gmail.com>
2021-11-01 11:22:26 -0400

    Fixed error

--- recipe/recipe-data/src/main/java/ca/gbc/comp3095/recipe/repositories/UserRepository.java
+++ recipe/recipe-data/src/main/java/ca/gbc/comp3095/recipe/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package ca.gbc.comp3095.recipe.repositories;
 
 import ca.gbc.comp3095.recipe.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User getUserByUsername(@Param("username") String username);
 }
